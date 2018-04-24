42371b0f11fda7c6dabedb9b4565f8aa117a6d3a
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-24 19:36:58 +0100

    Added basic constructor to SongContainer

--- uktoptwenty/src/containers/SongContainer.js
+++ uktoptwenty/src/containers/SongContainer.js
@@ -2,6 +2,14 @@ import React from "react";
 
 class SongContainer extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      songs: [],
+      currentSong: null
+    };
+  }
+
   render(){
     return (
       <h1>Song Container</h1>
