d3eafebffc9ae18ac1081d605118b30d26a84a01
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-24 19:33:43 +0100

    Added skeleton SongContainer container and rendered this in App.js

---
+++ uktoptwenty/src/containers/SongContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class SongContainer extends React.Component {
+
+  render(){
+    return (
+      <h1>Song Container</h1>
+    )
+  }
+}
+
+export default SongContainer;
